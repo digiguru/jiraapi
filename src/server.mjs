61f816736e91b234240695fd34b10c66119c5c83
Adam Hall <adam.hall@hitched.email>
2018-06-19 19:23:29 +0100

    Allow headers for username and password for CORs

--- src/server.mjs
+++ src/server.mjs
@@ -13,7 +13,7 @@ const requestHandler = async (request, response) => {
   //}
 	response.setHeader('Access-Control-Request-Method', '*');
 	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-	response.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+	response.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, username, password');
   
   
   if ( request.method === 'OPTIONS' ) {
